b946836c9d192bb6df84a52741a371240c965b31
Weasell <30350166+Weasell@users.noreply.github.com>
2022-08-25 11:18:27 -0400

    upload files to the folder

---
+++ rowData/row_data.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    feather.replace();
+
+     $.ajax({
+      url:"../climate.csv",
+      dataType:"text",
+      success:function(data)
+      {
+       var employee_data = data.split(/\r?\n|\r/);
+       var table_data = '<table style="margin-top: 10px;" class="table table-bordered table-striped">';
+       for(var count = 0; count<employee_data.length; count++)
+       {
+        var cell_data = employee_data[count].split(",");
+        table_data += '<tr>';
+        for(var cell_count=0; cell_count<cell_data.length; cell_count++)
+        {
+         if(count === 0)
+         {
+          table_data += '<th>'+cell_data[cell_count]+'</th>';
+         }
+         else
+         {
+          table_data += '<td>'+cell_data[cell_count]+'</td>';
+         }
+        }
+        table_data += '</tr>';
+       }
+       table_data += '</table>';
+       $('#showTable').html(table_data);
+      }
+     });    
+   });
